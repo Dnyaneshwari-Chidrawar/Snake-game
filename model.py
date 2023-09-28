4008506a43e7c8508db4517eb44b9d139b693437
Dnyaneshwari-Chidrawar <d.chidrawar1@gmail.com>
2023-09-28 21:05:12 +0530

    Added 3 new model

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("adding snake game")
+
+def model1():
+    print('Adding code for snake game 1')
+
+def model2():
+    print('Adding code for snake game 2')
+
+def model3():
+    print('Adding code for snake game 3')
