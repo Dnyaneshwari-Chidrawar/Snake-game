6e4534d028e6e1481769e754ce2b250e2ada3a1c
Dnyaneshwari-Chidrawar <d.chidrawar1@gmail.com>
2023-09-28 21:24:38 +0530

    added on model 2

--- model.py
+++ model.py
@@ -3,8 +3,6 @@ print("adding snake game")
 def model1():
     print('Adding code for snake game 1')
 
-def model2():
-    print('Adding code for snake game 2')
 
 
 print("Snake game 2 is performing good")
